07cc14dbdb623166311155e5116f382b7ba36673
tlsgnvkr <97785040+tlsgnvkr@users.noreply.github.com>
2024-09-24 19:21:47 +0900

    Create #3 BOJ #15486.cpp

---
+++ Week01/#3 BOJ #15486.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> t(n + 1, 0);
+	vector<int> p(n + 1, 0);
+	vector<int> dp(n + 2, 0);
+	for (int i = 1; i <= n; i += 1) cin >> t[i] >> p[i];
+
+	int res = 0;
+	for (int i = 1; i <= n; i += 1) {
+		dp[i] = max(dp[i], res);
+		if (i + t[i] <= n + 1) dp[i + t[i]] = max(dp[i + t[i]], dp[i] + p[i]);
+		res = max(res, dp[i]);
+	}
+
+	res = max(res, dp[n + 1]);
+	cout << res << endl;
+	return 0;
+}
